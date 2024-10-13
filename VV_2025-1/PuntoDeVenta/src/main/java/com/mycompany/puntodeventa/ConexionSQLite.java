e64ca038eb5455bbf3c5a2eed51f7d891d776b4b
Robhood98 <146679799+Robhood98@users.noreply.github.com>
2024-10-12 20:17:09 -0600

    Tareas

---
+++ VV_2025-1/PuntoDeVenta/src/main/java/com/mycompany/puntodeventa/ConexionSQLite.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.puntodeventa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class ConexionSQLite {
+    
+    private static Connection conexion = null;
+    private static final String URL = "jdbc:sqlite:puntodeventa.sqlite";
+        
+    private ConexionSQLite() {}
+    
+    public static Connection conectar(){
+        if( conexion == null ){
+            try {
+                conexion = DriverManager.getConnection(URL);
+                
+                crearTablaUsuarios();
+            } catch (SQLException ex) {
+                Logger.getLogger(ConexionSQLite.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        return conexion;
+    }
+    
+    public static void cerrarConexion(){
+        if( conexion != null){
+            try {
+                conexion.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ConexionSQLite.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public static void crearTablaUsuarios(){
+        try (Statement statement = conexion.createStatement()){
+            String sql = "CREATE TABLE IF NOT EXISTS usuarios ("
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "nombre TEXT NOT NULL,"
+                    + "correo TEXT NOT NULL UNIQUE,"
+                    + "contra TEXT NOT NULL,"
+                    + "rol TEXT NOT NULL"
+                    + ")";
+            
+            statement.execute(sql);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionSQLite.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ VV_2025-1/PuntoDeVenta/src/main/java/com/mycompany/puntodeventa/PuntoDeVenta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.puntodeventa;
+
+/**
+ *
+ * @author chris
+ */
+public class PuntoDeVenta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
